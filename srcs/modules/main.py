f3bdd409d46f265da05622524b585b02306b750a
Seo-yeon <cdawn0808@gmail.com>
2023-09-05 16:01:29 +0900

    feat: html pages

---
+++ srcs/modules/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import pymysql
+from pydantic import BaseModel
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+@app.get("/popup.html")
+async def popup(request: Request):
+    return  templates.TemplateResponse("popup.html",{"request":request})
